7430d2873edf07f270d7a89f27844c352f3b11f6
Claire Martinez <cmartinez@quickbase.com>
2017-06-29 14:05:24 -0400

    add imports commented out so others can experiment with it.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
 
+// import Config from "./examples/Config";
+// import Gallery from "./examples/Gallery";
+// import RecursiveProgramatic from "./examples/RecursiveProgramatic";
+// import CustomLink from "./examples/CustomLink";
+// import NoMatch from "./examples/NoMatch";
+// import PreventLoss from "./examples/PreventLoss";
+// import Auth from "./examples/Auth";
+// import Recursive from "./examples/Recursive";
+// import Sidebar from "./examples/Sidebar";
+// import Animation from "./examples/Animation";
+// import Ambig from "./examples/Ambig";
+// import PreventLossCustom from './examples/PreventLossCustom';
 import Basic from './examples/Basic';
 
 class App extends Component {
